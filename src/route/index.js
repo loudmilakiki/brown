a96f0484a1a92b846a93e6a2c9a02bf479545c87
loudmilakiki <120623945+loudmilakiki@users.noreply.github.com>
2023-10-28 17:16:41 +0300

    brown

--- src/route/index.js
+++ src/route/index.js
@@ -16,6 +16,8 @@ router.get('/', function (req, res) {
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+
+    // component: [header],
   })
   // ↑↑ сюди вводимо JSON дані
 })
@@ -37,7 +39,71 @@ router.get('/eyelashes', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
+//============================================
+
+router.get('/build-eyelashes', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('build-eyelashes', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'build-eyelashes',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/coloring', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('coloring', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'coloring',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/spa', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('spa', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'spa',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/removal', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('removal', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'removal',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/build-eyelashes', function (req, res) {})
+//=======================================
+
+router.post('/eyelashes', function (req, res) {})
+
 // ================================================================
+
+router.get('/brows', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('brows', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'brows',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/brows', function (req, res) {})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
